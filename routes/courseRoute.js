568e68fcc3d6cda8e569c1f55dd62e0a93ccb2f1
Sanketh-Ganiga <sankethganiga888@gmail.com>
2023-07-23 14:17:09 +0530

    multiple category selection added

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -22,9 +22,21 @@ courseRouter.get("/",async(req,res)=>{
         if(search){
             query.title={$regex: search, $options:"i"}
         }
-        if(category){
-            query.category={$regex:category,$options:"i"}
-        }
+       
+       if(category){
+    let cat=typeof category
+    console.log(cat)
+if (cat==="object") {
+  console.log("helo")
+  query.category ={ $in: [...category] };
+ 
+
+} else if(cat==="string"){
+  query.category =  category;
+  console.log(query)
+}
+      }
+        
         if(minRating && maxRating){
           query.$and=[
             {rating:{$gte:minRating}},
@@ -41,9 +53,11 @@ courseRouter.get("/",async(req,res)=>{
         else if(sortBy==="desc"){sortOrder=-1}
         
         // const toSkip=pageNo*(perPage-1)
+        
+    
+       console.log(query)
        
-       
-    const course= await CourseModel.find(query,projection).sort({[field1]:sortOrder}).skip(pageNo > 0 ? ( ( pageNo - 1 ) * perPage ) : 0).limit(perPage)
+    const course= await CourseModel.find(query).sort({[field1]:sortOrder}).skip(pageNo > 0 ? ( ( pageNo - 1 ) * perPage ) : 0).limit(perPage)
     res.send({total:course.length,course})
    }
    catch(err){
